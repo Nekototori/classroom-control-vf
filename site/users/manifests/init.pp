e8fa3d2ceb15944559e4cfa0a93f4da9e76ed822
jionaj <31247279+jionaj@users.noreply.github.com>
2017-08-22 15:43:44 -0400

    Update init.pp

--- site/users/manifests/init.pp
+++ site/users/manifests/init.pp
@@ -1,3 +1,7 @@
-user {'fundamentals':
+class users {
+  user { 'fundamentals':
     ensure => present,
-}
+    'puppet8#labs', # Windows requires a plain text password
+    groups => ['Users'], # Display in Windows Control Panel
+    }
+  }
