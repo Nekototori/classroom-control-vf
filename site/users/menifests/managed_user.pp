d153f93887bda0f8bda8421c8b7a0ce077ed3f4c
tzericli <31246252+tzericli@users.noreply.github.com>
2017-08-23 14:42:44 -0400

    Update managed_user.pp

--- site/users/menifests/managed_user.pp
+++ site/users/menifests/managed_user.pp
@@ -2,7 +2,10 @@ define users::managed_user (
     $managed_user_group = $title,
 ) {
 
-  user { $title
+  user { $title:
+    groups => $managed_user_group,
+  }  
+    
   file { ["/home/${title}","/home/${title}/.ssh"]:
     ensure  => directory,
     owner   => '${title}',
