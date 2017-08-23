0984914c614467104f87e7f15b59e7bab78b112f
Eric M. Jackson <ericmjackson@gmail.com>
2017-08-23 13:34:20 -0500

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,1 +1,20 @@
-
+define users::managed_user 
+  (#managed_user_group = $title)
+  {
+  user { $title:
+    ensure => present,
+    group => $managed_user_group,
+  }
+  
+  file { "/home/$title":
+    ensure => directory,
+    owner => $title,
+    group => $title,
+    mode = '0775',
+  }
+  
+  group { $managed_user_group:
+    ensure => present,
+  }
+    
+}
