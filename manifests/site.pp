fd70b8f13e4bacdac93a2d5ea47f34aba0fa3720
Nekototori <jessykah.bird@gmail.com>
2017-08-23 08:52:06 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,18 +44,6 @@ node default {
   #   class { 'my_class': }
   include role::classroom
   
-  file { '/etc/motd':
-     ensure => file,
-     group  => 'root',
-     owner  => 'root',
-     mode   => '0644',
-     content => 'I learned how to break things!',
-   }
-  
-  include users
-  include skeleton
-  
-  
   # This ensures that the skeleton class happens before the users class.
   Class['skeleton'] -> Class['users']
     
