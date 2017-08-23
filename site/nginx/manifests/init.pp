93e9670a0d7ccd0a156748a629d96671e120ac08
tzericli <31246252+tzericli@users.noreply.github.com>
2017-08-23 17:41:30 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -7,6 +7,9 @@ class nginx {
   package { 'nginx':
     ensure => present,
   }
+  file { '/var/www/':
+    ensure => directory,
+  }  
   file { '/var/www/index.html':
     ensure => file,
     source => 'puppet:///modules/nginx/index.html',
