11d56ef2edcaf50abbd814d845499e0dec3255d3
csheehangpc <31245749+csheehangpc@users.noreply.github.com>
2017-08-23 11:36:44 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,4 +1,8 @@
 class nginx {
+File {
+  owner  => 'root',
+  group  => 'root',
+}
 
   package { 'nginx':
     ensure => present,
@@ -10,15 +14,11 @@ class nginx {
   
   file { '/var/www/index.html':
     ensure => file,
-    owner  => 'root',
-    group  => 'root',
     source => 'puppet:///modules/nginx/index.html',
   }
   
   file { '/etc/nginx/nginx.conf':
     ensure => file,
-    owner  => 'root',
-    group  => 'root',
     source => 'puppet:///modules/nginx/nginx.conf',
   }
   
@@ -28,8 +28,6 @@ class nginx {
   
   file { '/etc/nginx/conf.d/default.conf':
     ensure => file,
-    owner  => 'root',
-    group  => 'root',
     source => 'puppet:///modules/nginx/default.conf',
   }
   
