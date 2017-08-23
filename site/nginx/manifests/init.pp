a27b84469908e82501cc0337d9002c2db17808ed
rhymemime <bramsey967@gmail.com>
2017-08-23 08:18:09 -0500

    created init.pp for nginx

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,35 @@
+class nginx{
+
+  package{'nginx':
+    ensure => present
+  }
+  file {'/var/www':
+    ensure => directory,
+  }
+  file {'/var/www/index.html':
+    ensure => file
+    onwer  => 'root',
+    groups => 'root',
+    source => 'puppet:///modules/nginx/index.html',
+  }
+
+  file {'/etc/nginx/nginx.conf':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    source => 'puppet:///modules/nginx/nginx.conf
+  }
+  file {'/etc/nginx/conf.d':
+    ensure => directory,
+  }
+  file {'/etc/nginx/conf.d/default.conf':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    source => 'puppet:///modules/nginx/nginx.conf
+  }
+  service{ 'nginx':
+    ensure => 'running',
+    enable => true,
+  }
+}
