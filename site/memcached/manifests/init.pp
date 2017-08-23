1efdf03ceb779dcc12cdc5d56b4109bd72c95be4
Eric M. Jackson <ericmjackson@gmail.com>
2017-08-23 10:10:04 -0500

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,1 +1,18 @@
-#New init
+class memcached {
+
+  package { 'memcached':
+    ensure => present,
+  }
+
+  file { '/etc/sysconfig/memcached':
+    ensure => file,
+    source => 'puppet:///module/memcached/memcached',
+    require => Package['memcached'],
+  }
+
+  service { 'memcached':
+    ensure => running,
+    enabled => true,
+    subscribe => File[`/etc/sysconfig/memcached`],
+  } 
+}
