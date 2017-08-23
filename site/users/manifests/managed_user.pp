31538a8a158bbe5c81431ee4a0206aba45029035
csheehangpc <31245749+csheehangpc@users.noreply.github.com>
2017-08-23 14:33:53 -0400

    Create managed_user.pp

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,21 @@
+define users::managed_user (
+  $managed_user_group = $title,
+) {
+
+  user { $title:
+    group  => $managed_user_group
+    ensure => present,
+  }
+  
+  file { ["/home/${title}", "/home/${title}/.ssh"]:
+    ensure => directory,
+    owner  => $title,
+    group  => $managed_user_group,
+    mode   => '0755',
+  }
+  
+  group { $managed_user_group:
+    ensure => present,
+  }
+  
+}
