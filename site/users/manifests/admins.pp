be983533add18e99e5653b605a9d5dde873b7dbe
Jessykah Bird <jessykah@puppet.com>
2017-08-23 13:55:04 -0500

    Fixed syntax

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,9 +1,9 @@
 class users::admin {
   users::managed_user { "Jose": }
   
-  users::managed_user { "Alice": }
+  users::managed_user { "Alice": 
     managed_user_group => 'Admins',
-  
+  }
   users::managed_user { "Chen": }
 
 }
--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,16 +1,16 @@
-define users::managed_user 
-  (#managed_user_group = $title)
-  {
+define users::managed_user (
+  $managed_user_group = $title,
+) {
   user { $title:
     ensure => present,
     group => $managed_user_group,
   }
   
-  file { "/home/$title":
+  file { "/home/${title}":
     ensure => directory,
     owner => $title,
     group => $title,
-    mode = '0775',
+    mode => '0775',
   }
   
   group { $managed_user_group:
