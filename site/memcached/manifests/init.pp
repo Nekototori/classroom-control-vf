27e70a32c3f86a3b5dbc0ea618b2dab69d2727e5
Kit <kit@mkso.net>
2017-08-23 10:38:20 -0400

    switch back to source config file

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -9,12 +9,12 @@ ensure => file,
 owner => 'root',
 group => 'root',
 mode => '0644',
-#source => 'puppet:///modules/memcached/memcached',
-content => 'PORT="11211"
-USER="memcached"
-MAXCONN="96"
-CACHESIZE="32"
-OPTIONS=""',
+source => 'puppet:///modules/memcached/memcached',
+#content => 'PORT="11211"
+#USER="memcached"
+#MAXCONN="96"
+#CACHESIZE="32"
+#OPTIONS=""',
 require => Package['memcached'],
 } 
 
