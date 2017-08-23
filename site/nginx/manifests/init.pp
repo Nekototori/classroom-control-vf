13fb8ac46d5428b128c4ea878e6435801fcb8677
gschaag <31130778+gschaag@users.noreply.github.com>
2017-08-23 08:44:37 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,13 +1,29 @@
 class nginx {
 
-package { 'nginx': 
-  ensure => installed, 
-  name => $ngin
-  }
-  
+file { '/etc/nginx/nginx.conf',
+ensure =>file,
+owner => 'root',
+group => 'root',
+source => 'puppet:///modules/nginx/files/nginx.conf',
+}
+file { '/var/wwww':
+ensure => directory,
+owner => 'root',
+group => 'root',
+}
+file { '/var/www/index.html':
+ensure => file,
+owner => 'root',
+group => 'root',
+source => 'puppet:///modules/nginx/files/index.html',
+}
+package { 'nginx':
+ensure => installed,
+name => $ngin
+}
 service { 'nginx':
-  ensure => 'running', 
-  enable => 'true',
+ensure => 'running',
+enable => 'true',
+}
   
-  }  
 }
