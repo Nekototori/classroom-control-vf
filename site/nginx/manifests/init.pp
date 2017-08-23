bf5f0ae2ee63ada6487f10989d7d21128bf82e49
tzericli <31246252+tzericli@users.noreply.github.com>
2017-08-23 17:25:42 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,12 +2,32 @@ class nginx {
   File {
     owner => 'root',
     group => 'root',
-    mode => '6044',
+    mode => '0644',
   }
   package { 'nginx':
-    ensure => installed,
-    }
-  
-  file { '/etc/sysconfig/nginx.conf'
+    ensure => present,
   }
+  file { '/var/www/index.html':
+    ensure => file,
+    source => 'puppet:///module/nginx/index.html',
+  }
+  file { '/etc/nginx/nginx.conf':
+    ensure => file,
+    source => 'puppet:///module/nginx/nginx.conf',
+    require => Package['nginx'],
+    notify => Service['nginx'],
+  }
+  file { '/etc/nginx/conf.d':
+    ensure => directory,
+  }
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure => file,
+    source => 'puppet:///module/nginx/default.conf',
+    require => Package['nginx'],
+    notify => Service['nginx'],
+  }
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+  }  
 }
