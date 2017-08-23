e0e667da99d7c6289da729d127a54d5e81637fc7
Matt Coates <me@mattjackets.com>
2017-08-23 16:17:38 -0400

    Multi-platform support for nginx

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,42 +1,77 @@
 class nginx {
+
+  $service_name = 'nginx'
+
+  case $facts['os']['family'] {
+    'windows' : {
+      $package_name = 'nginx-service',
+      $file_owner = 'Administrator',
+      $file_group = 'Administrators',
+      $document_root = 'C:/ProgramData/nginx/html',
+      $config_directory = 'C:/ProgramData/nginx',
+      $server_block_directory = 'C:/ProgramData/nginx/conf.d',
+      $logs_directory = 'C:/ProgramData/nginx/logs',
+    }
+    'debian','redhat' : {
+      $package_name = 'nginx',
+      $file_owner = 'root',
+      $file_group = 'root',
+      $document_root = '/var/www',
+      $config_directory = '/etc/nginx',
+      $server_block_directory = '/etc/nginx/conf.d',
+      $logs_directory = '/var/log/nginx',
+     }
+     default : { fail("${facts['os']['family']} is not supported by this module: ${module_name}")}
+  }
+
+  $user_service_run_as = $facts['os']['family'] ? {
+    'redhat' => 'nginx',
+    'debian' => 'www-data',
+    'windows' => 'nobody'
+  }
+
   File {
-    owner  => 'root',
-    group  => 'root',
+    owner  => $file_owner,
+    group  => $file_group,
     mode => '0644'
   }
 
-  package { 'nginx':
+  package { $package_name:
     ensure => present,
   }
 
-  file { '/var/www':
+  file { $document_root:
     ensure => directory,
   }
   
-  file { '/var/www/index.html':
+  file { "${document_root}/index.html":
     ensure => file,
     source => 'puppet:///modules/nginx/index.html',
   }
   
-  file { '/etc/nginx/nginx.conf':
+  file { "${config_directory}/nginx.conf":
     ensure => file,
-    source => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify => Service['nginx'],
+    content => epp('nginx/nginx.conf.epp', {
+                    user_service_run_as => $user_service_run_as,
+		    config_directory => $config_directory,
+		    logs_directory => $logs_directory
+		    })
   }
   
-  file { '/etc/nginx/conf.d':
+  file { $server_block_directory :
     ensure => directory,
   }
   
-  file { '/etc/nginx/conf.d/default.conf':
+  file { "${server_block_directory}/default.conf":
     ensure => file,
-    source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
     notify => Service['nginx'],
+    content => epp('nginx/default.conf.epp', { document_root => $document_root })
   }
   
-  service { 'nginx':
+  service { $service_name:
     ensure => 'running',
     enable => true,
   }
