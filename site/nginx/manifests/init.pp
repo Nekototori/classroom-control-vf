b1f7b141904b37b038026e12b8bbd3d627b50182
tzericli <31246252+tzericli@users.noreply.github.com>
2017-08-23 18:36:47 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,34 +1,67 @@
 class nginx {
+  case $facts['os']['family'] {
+    'redhat', 'debian': {
+      $package = 'nginx'
+      $owner = 'root'
+      $group = 'root'
+      $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $logdir = '/var/log/nginx' 
+    }
+    'windows': {
+      $package = 'nginx-service',
+      $owner = 'Administrator',
+      $group = 'Administrator',
+      $docroot = 'C:/ProgramData/nginx/html'
+      $confdir = 'C:/ProgramData/nginx'
+      $logdir = 'C:/ProgramData/nginx/logs'
+    }
+    default: {
+      fail ("Module ${module_name} is not supported on plateform ${facts['os']['family']}")
+    }
+  }
+  
+  $user = $facts['os']['family'] ? {
+    'redhat' => 'nginx',
+    'debian' => 'www-data',
+    'windows' => 'nobody',
+  }
+  
   File {
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
+  owner => $owner,
+  group => $group,
+  mode => '0664',
   }
   package { 'nginx':
     ensure => present,
   }
-  file { '/var/www/':
-    ensure => directory,
-  }  
-  file { '/var/www/index.html':
-    ensure => file,
-    source => 'puppet:///modules/nginx/index.html',
+  
+  file { [ $docroot, "${confdir}/conf.d" ]:
+  ensure => directory,
   }
-  file { '/etc/nginx/nginx.conf':
-    ensure => file,
-    source => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
-    notify => Service['nginx'],
+  file { "${docroot}/index.html":
+  ensure => file,
+  source => 'puppet:///modules/nginx/index.html',
   }
-  file { '/etc/nginx/conf.d':
-    ensure => directory,
+  file { "${confdir}/nginx.conf":
+  ensure => file,
+  content => epp('nginx/nginx.conf.epp',
+  {
+  user => $user,
+  confdir => $confdir,
+  logdir => $logdir,
+  }),
+  notify => Service['nginx'],
   }
-  file { '/etc/nginx/conf.d/default.conf':
-    ensure => file,
-    source => 'puppet:///modules/nginx/default.conf',
-    require => Package['nginx'],
-    notify => Service['nginx'],
+  file { "${confdir}/conf.d/default.conf":
+  ensure => file,
+  content => epp('nginx/default.conf.epp',
+  {
+  docroot => $docroot,
+  }),
+  notify => Service['nginx'],
   }
+  
   service { 'nginx':
     ensure => running,
     enable => true,
