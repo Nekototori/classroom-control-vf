0d537b412b852042558581a07db2af57cedaa8b1
gschaag <31130778+gschaag@users.noreply.github.com>
2017-08-24 09:55:29 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -20,7 +20,6 @@ case $facts['os']['family'] {
 default : {
 fail("Module ${module_name} is not supported on ${facts['os']['family']}")
 }
-}
 # user the service will run as. Used in the nginx.conf.epp template
 $user = $facts['os']['family'] ? {
 'redhat' => 'nginx',
