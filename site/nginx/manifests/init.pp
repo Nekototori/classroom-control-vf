121fb896ddbc7f61ab2cf62b959c09969e54a869
rhymemime <bramsey967@gmail.com>
2017-08-24 10:46:06 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,12 +1,12 @@
 class nginx(
-  $root
+  $root = undef,
 ){
   case $facts['os']['family'] {
     'redhat','debian' : {
       $package = 'nginx'
       $owner = 'root'
       $group = 'root'
-      $docroot = '/var/www'
+      #$docroot = '/var/www'
       $confdir = '/etc/nginx'
       $logdir = '/var/log/nginx'
       $service = 'nginx'
@@ -15,7 +15,7 @@ class nginx(
       $package = 'nginx-service'
       $owner = 'Administrator'
       $group = 'Administrators'
-      $docroot = 'C:/ProgramData/nginx/html'
+      #$docroot = 'C:/ProgramData/nginx/html'
       $confdir = 'C:/ProgramData/nginx'
       $logdir = 'C:/ProgramData/nginx/logs'
     }
