1f067819b81f7ae0af058879f28492fd843eb157
gschaag <31130778+gschaag@users.noreply.github.com>
2017-08-24 11:54:41 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,32 +1,13 @@
-class nginx {
-
-case $facts['os']['family'] {
-  'redhat','debain' : {
-  $package = 'nginx'
-  $owner = 'root'
-  $group = 'root'
-  $docroot = '/var/www'
-  $configdir = '/etc/nginx'
-  $logdir = '/var/log/nginx'
-  }
-'windows' : {
-  $package = 'nginx-service'
-  $owner = 'Administrator'
-  $group = 'Administrators'
-  $docroot = 'C:/ProgramData/nginx/html'
-  $configdir = 'C:/ProgramData/nginx'
-  $logdir = 'C:/ProgramData/nginx/logs'
-  }
-default : {
-fail("Module ${module_name} is not supported on ${facts['os']['family']}")
-  }
-}
-# user the service will run as. Used in the nginx.conf.epp template
-$user = $facts['os']['family'] ? {
-  'redhat' => 'nginx',
-  'debian' => 'www-data',
-  'windows' => 'nobody',
-}
+class nginx (
+$package = $nginx::params::package,
+$owner = $nginx::params::owner,
+$group = $nginx::params::group,
+$docroot = $nginx::params::docroot,
+$confdir = $nginx::params::confdir,
+$logdir = $nginx::params::logdir,
+$user = $nginx::params::user,
+$port = $nginx::params::port
+) inherits nginx::params {
 
 File {
   owner  => $owner,
