33b1d78941bd29b86fdeb69b812e0fa04b0cf94e
rhymemime <bramsey967@gmail.com>
2017-08-24 10:51:38 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -10,6 +10,8 @@ class nginx(
       $confdir = '/etc/nginx'
       $logdir = '/var/log/nginx'
       $service = 'nginx'
+      
+      $default_docroot = '/var/www'
     }
     'windows' : {
       $package = 'nginx-service'
@@ -18,6 +20,8 @@ class nginx(
       #$docroot = 'C:/ProgramData/nginx/html'
       $confdir = 'C:/ProgramData/nginx'
       $logdir = 'C:/ProgramData/nginx/logs'
+      
+      $default_docroot = ''C:/ProgramData/nginx/html'
     }
     default : {
       fail("Module ${module_name} is not supported on ${facts['os']['family']}")
@@ -28,6 +32,12 @@ class nginx(
     'debian' => 'www-data',
     'windows' => 'nobody',
   }
+  
+  $docroot = $root ? {
+    undef => $default_docroot,
+    default => $root,
+  }
+
 
   File{
     owner => $owner,
