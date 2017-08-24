44469395726d8d65b94446518d33f506e23269ce
gschaag <31130778+gschaag@users.noreply.github.com>
2017-08-24 12:30:37 -0400

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -1,30 +1,41 @@
 class nginx::params {
 
   case $facts['os']['family'] {
-    'redhat','debian' : {
-      $package = 'nginx'
-      $owner = 'root'
-      $group = 'root'
-      $docroot = '/var/www'
-      $configdir = '/etc/nginx'
-      $logdir = '/var/log/nginx'
-}
-  'windows' : {
-    $package = 'nginx-service'
-    $owner = 'Administrator'
-    $group = 'Administrators'
-    $docroot = 'C:/ProgramData/nginx/html'
-    $configdir = 'C:/ProgramData/nginx'
-    $logdir = 'C:/ProgramData/nginx/logs'
-}
-default : {
-fail("Module ${module_name} is not supported on ${facts['os']['family']}")
-}
-}
-$user = $facts['os']['family'] ? {
-  'redhat' => 'nginx',
-  'debian' => 'www-data',
-  'windows' => 'nobody',
+  
+  'redhat','debain' : { 
+    $package = 'nginx' 
+    $owner = 'root' 
+    $group = 'root' 
+    $docroot = '/var/www' 
+    $configdir = '/etc/nginx' 
+    $logdir = '/var/log/nginx' 
+    }
+     'windows' : { 
+        $package = 'nginx-service' 
+        $owner = 'Administrator' 
+        $group = 'Administrators' 
+        $docroot = 'C:/ProgramData/nginx/html' 
+        $configdir = 'C:/ProgramData/nginx' 
+        $logdir = 'C:/ProgramData/nginx/logs' 
+        } 
+        
+        default : { 
+        fail("Module ${module_name} is not supported on ${facts['os']['family']}") 
+          } 
+        # user the service will run as. Used in the nginx.conf.epp template 
+        
+        $user = $facts['os']['family'] ? { 
+          'redhat' => 'nginx', 
+          'debian' => 'www-data', 
+          'windows' => 'nobody', 
+          } 
+          
+          File { 
+           owner  => $owner, 
+           group => $group, 
+           mode => '0644', 
+               } 
+
 }
 $port = '80'
 }
