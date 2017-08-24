7200cef470e2cd3b189a26efab55a69645a60830
gschaag <31130778+gschaag@users.noreply.github.com>
2017-08-24 09:49:13 -0400

    template

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,45 +1,67 @@
 class nginx {
 
 case $facts['os']['family'] {
-'redhat','debain' : {
-$package = 'nginx'
-$owner = 'root'
-$group = 'root'
-$docroot = '/var/www'
-$configdir = '/etc/nginx'
+  'redhat','debain' : {
+  $package = 'nginx'
+  $owner = 'root'
+  $group = 'root'
+  $docroot = '/var/www'
+  $configdir = '/etc/nginx'
+  $logdir = '/var/log/nginx'
 }
 'windows' : {
-
+  $package = 'nginx-service'
+  $owner = 'Administrator'
+  $group = 'Administrators'
+  $docroot = 'C:/ProgramData/nginx/html'
+  $configdir = 'C:/ProgramData/nginx'
+  $logdir = 'C:/ProgramData/nginx/logs'
 }
+default : {
+fail("Module ${module_name} is not supported on ${facts['os']['family']}")
+}
+}
+# user the service will run as. Used in the nginx.conf.epp template
+$user = $facts['os']['family'] ? {
+'redhat' => 'nginx',
+'debian' => 'www-data',
+'windows' => 'nobody',
+}
+
 File {
-  owner  => 'root',
-  group => 'root',
+  owner  => $owner,
+  group => $group,
   mode => '0644',
     }
 
 
-file { '/etc/nginx/nginx.conf',
-source => 'puppet:///modules/nginx/nginx.conf',
+file { [ ${docroot}, "${configdir}/conf.d"]:
+  ensure =>directory,
 }
 file { '/var/wwww':
 ensure => directory,
 
 }
-file { '/var/www/index.html':
-source => 'puppet:///modules/nginx/index.html',
+file { "$docroot}/index.html":
+  ensure => file,
+  source => 'puppet:///modules/nginx/index.html',
 }
 
-file { '/etc/nginx/conf.d',
-  ensure => directory
+file { ${configdir}/nginx.conf":
+  ensure => file
+  content => epp('nginx.nginx.conf.epp
+    {
+        user => $user,
+        configdir => %configdir,
+        logdir => $logdir,
+        }),
+     notifty => Service['nginx'],
   }
   
-  file { '/etc/nginx/conf.d/default.conf':
-    source => puppet:///modules/nginx/default.conf
+  file { "${configdir}/conf.d/default.conf":
+    ensure => file
+    content => epp('nginx/default.conf.epp
   }
-package { 'nginx':
-ensure => installed,
-
-}
 service { 'nginx':
 ensure => 'running',
 enable => 'true',
