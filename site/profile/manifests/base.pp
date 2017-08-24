3f84e2e7059f071b5edfa4ce8dd1a28f1c23528a
Nekototori <jessykah.bird@gmail.com>
2017-08-24 14:09:35 -0500

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -3,4 +3,12 @@ class profile::base (
 ) {
   notify { $message: }
   
+  
+  include ::redis
+  include epel
+  limits::fragment { "*/soft/nofile":
+   value => "1024";
+ "*/hard/nofile":
+   value => "8192";
+  }
 }
