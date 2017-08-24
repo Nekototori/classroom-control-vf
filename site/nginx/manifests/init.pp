efae3e4fb3388ae99d87482cb8894975d6765aa0
Nekototori <jessykah.bird@gmail.com>
2017-08-24 11:24:43 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,43 +1,7 @@
 class nginx (
   $root = undef,
 ) {
-  case $facts['os']['family'] {
-    'RedHat' : {
-      $service_user = 'nginx'
-      $config_dir = '/etc/nginx'
-      $log_dir = '/var/log/nginx'
-      $default_docroot = '/var/www'
-      $file_owner = 'root'
-      $file_group = 'root'
-      $package = 'nginx'
-    }
-    'debian' : {
-      $service_user = 'www-data'
-      $config_dir = '/etc/nginx'
-      $log_dir = '/var/log/nginx'
-      $default_docroot = '/var/www'
-      $file_owner = 'root'
-      $file_group = 'root'
-      $package = 'nginx'
-    }
-    'windows' : {
-      $service_user = 'nobody'
-      $config_dir = 'C:/ProgramData/nginx'
-      $log_dir = 'C:/ProgramData/nginx/logs'
-      $default_docroot = 'C:/ProgramData/nginx/html'
-      $file_owner = 'Administrator'
-      $file_group = 'Administrators'
-      $package = 'nginx-service'
-    }
-    default : {
-      fail( "The os family ${facts['os']['family']} is not supported.")
-   }
- }
   
-  $docroot = $root ? {
-    undef => $default_docroot,
-    default => $root,
-  }
   
   File {
     owner => $file_owner,
