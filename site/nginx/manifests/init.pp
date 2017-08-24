d3072d9f92f325ee075b7e6cae44d9cf3b1d33ae
Kit <kit@mkso.net>
2017-08-24 12:20:25 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,53 +1,15 @@
 class nginx (
-  $root = undef,
-){
+$package = $nginx::params::package,
+$owner = $nginx::params::owner,
+$group = $nginx::params::group,
+$docroot = $nginx::params::docroot,
+$confdir = $nginx::params::confdir,
+$logdir = $nginx::params::logdir,
+$user = $nginx::params::user,
+$port = $nginx::params::port
+) inherits nginx::params {
 
 notify {"Platform OS is: ${facts['os']['family']}":}
-
-case $facts['os']['family'] {
-
-'RedHat','debian' : {
-$package = 'nginx'
-$owner = 'root'
-$group = 'root'
-#$docroot = '/var/www'
-$confdir = '/etc/nginx'
-$logdir = '/var/log/nginx'
-$run_pid_path = '/var/run'
-# this will be used if we don't pass in a value
-$default_docroot = '/var/www'
-}
-
-'windows' : {
-$package = 'nginx-service'
-$owner = 'Administrator'
-$group = 'Administrators'
-#$docroot = 'C:/ProgramData/nginx/html'
-$confdir = 'C:/ProgramData/nginx'
-$logdir = 'C:/ProgramData/nginx/logs'
-$run_pid_path = 'C:/ProgramData/nginx/run'
-# this will be used if we don't pass in a value
-$default_docroot = 'C:/ProgramData/nginx/html'
-}
-
-default : {
-  fail("Module ${module_name} is not supported on ${facts['os']['family']}")
-}
-
-}
-
-#user the service will run as. Used in the nginx.conf.epp template
-$user = $facts['os']['family'] ? {
-'RedHat' => 'nginx',
-'debian' => 'www-data',
-'windows' => 'nobody',
-} 
-
-# if $root isn't set, then fall back to the platform default
-$docroot = $root ? {
-undef => $default_docroot,
-default => $root,
-}
 
 File {
 owner => $owner,
